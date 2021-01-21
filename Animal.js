ee815432cc0fa8e0dd778a12f0343b55c47b53ff
Mrs Sharmin Akter <zamalzamalhossain@gmail.com>
2021-01-21 11:00:30 +0100

    added file

---
+++ Animal.js
@@ -0,0 +1,44 @@
+var depth = 22;
+var animal = 0;
+if(depth<=10){
+    animal = depth * 50;
+}
+else if(depth<= 20){
+    var firstPart = 10 * 50;
+    var remaining = depth -10;
+    var secondPart = remaining * 100;
+    animal = firstPart + secondPart;
+}
+else{
+    var firstPart = 10 *50;
+    var secondPart = 10 * 100;
+    var remaining = depth - 20;
+    var thirdPart = remaining * 300;
+    animal = firstPart + secondPart + thirdPart;
+}
+console.log(animal);
+
+// another system-------------------------------
+
+function animalCalculator(depth){
+    var animal = 0;
+    if(depth<=10){
+        animal = depth * 50;
+    }
+    else if(depth<= 20){
+        var firstPart = 10 * 50;
+        var remaining = depth -10;
+        var secondPart = remaining * 100;
+        animal = firstPart + secondPart;
+    }
+    else{
+        var firstPart = 10 *50;
+        var secondPart = 10 * 100;
+        var remaining = depth - 20;
+        var thirdPart = remaining * 300;
+        animal = firstPart + secondPart + thirdPart;
+    }
+            return animal;
+}
+var total = animalCalculator(24);
+console.log(total);
